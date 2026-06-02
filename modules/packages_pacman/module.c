9f9ba1e519b1caf8e66647335cef357d6569b3c9
diviocity <diviocity@disroot.org>
2026-06-03 00:49:31 +0300

    hacky fix to exclude ALPM_DB_VERSION from the count

--- modules/packages_pacman/module.c
+++ modules/packages_pacman/module.c
@@ -2,7 +2,7 @@
 const char* packages_pacman_module_preset(char* result, size_t result_size) {
     DIR* dir = opendir("/var/lib/pacman/local");
     if (!dir) { snprintf(result, result_size, "?"); return result; }
-    int count = 0;
+    int count = -1;
     struct dirent* e;
     while ((e = readdir(dir)) != NULL)
         if (e->d_name[0] != '.') count++;
